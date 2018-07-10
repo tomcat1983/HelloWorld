ab7606ebadf2d42f7ad3128d7987508bb4c0f3ae
youngwoo.jin <tomcat1983@gmail.com>
2018-07-10 09:58:46 +0900

    first commit

---
+++ src/basic/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package basic.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!");
+	}
+
+	public void testFunction() {
+		System.out.println("Test Function!!!");
+	}
+}
---
+++ test/basic/helloworld/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package basic.helloworld;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
